b8dfb116c32eb62f58a42e1295724126f30fc1b9
Victor Smirnov <aist11@gmail.com>
2017-03-14 23:52:24 +0300

    Added tests from Boost Fiber

---
+++ tests/test_barrier_dispatch.cpp
@@ -0,0 +1,71 @@
+
+//          Copyright Oliver Kowalke 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+// This test is based on the tests of Boost.Thread
+
+#include <sstream>
+#include <string>
+
+#include <boost/test/unit_test.hpp>
+
+#include <dumbo/v1/fiber/all.hpp>
+
+int value1 = 0;
+int value2 = 0;
+
+void fn1( dumbo::v1::fibers::barrier & b) {
+    ++value1;
+    dumbo::v1::this_fiber::yield();
+
+    b.wait();
+
+    ++value1;
+    dumbo::v1::this_fiber::yield();
+    ++value1;
+    dumbo::v1::this_fiber::yield();
+    ++value1;
+    dumbo::v1::this_fiber::yield();
+    ++value1;
+}
+
+void fn2( dumbo::v1::fibers::barrier & b) {
+    ++value2;
+    dumbo::v1::this_fiber::yield();
+    ++value2;
+    dumbo::v1::this_fiber::yield();
+    ++value2;
+    dumbo::v1::this_fiber::yield();
+
+    b.wait();
+
+    ++value2;
+    dumbo::v1::this_fiber::yield();
+    ++value2;
+}
+
+void test_barrier() {
+    value1 = 0;
+    value2 = 0;
+
+    dumbo::v1::fibers::barrier b( 2);
+    dumbo::v1::fibers::fiber f1( dumbo::v1::fibers::launch::dispatch, fn1, std::ref( b) );
+    dumbo::v1::fibers::fiber f2( dumbo::v1::fibers::launch::dispatch, fn2, std::ref( b) );
+
+    f1.join();
+    f2.join();
+
+    BOOST_CHECK_EQUAL( 5, value1);
+    BOOST_CHECK_EQUAL( 5, value2);
+}
+
+boost::unit_test::test_suite * init_unit_test_suite( int, char* []) {
+    boost::unit_test::test_suite * test =
+        BOOST_TEST_SUITE("Boost.Fiber: barrier test suite");
+
+    test->add( BOOST_TEST_CASE( & test_barrier) );
+
+    return test;
+}
